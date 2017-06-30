bcb23591dabc39abe47c95779bd3b3024f4fe925
Evgen <evgen@mail.com>
2017-06-30 09:38:50 +0300

    prefinal2

--- config.rb
+++ config.rb
@@ -1,46 +1,4 @@
-# UIKit Compass/SASS configuration.
-# NOTE: grunt will take its compass options from this file.
-
-# Required
-# -----------------------------------------------------------------------------
-require 'compass'
-
-
-
-
-# Directory paths
-# -----------------------------------------------------------------------------
-
-sass_dir = 'sass'
-css_dir = 'css'
-images_dir = 'img'
-
-
-# Precision
-# -----------------------------------------------------------------------------
-Sass::Script::Number.precision = 5
-
-
-# Output Style
-# -----------------------------------------------------------------------------
-# You can select your preferred output style here (:expanded, :nested, :compact
-# or :compressed). :expanded is closest to Drupal coding standards and it is
-# not necessary to compress in the preprocessor since Drupal will do this for
-# us using its own aggregation and compression systems.
-output_style = :expanded
-
-
-# Assets
-# -----------------------------------------------------------------------------
-relative_assets = true
-
-
-# Line Comments
-# -----------------------------------------------------------------------------
-line_comments = false
-
-
-# Source maps
-# -----------------------------------------------------------------------------
-#sourcemap = true
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "css/images"
 
